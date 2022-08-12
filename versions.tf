ff35c5c5141d32acdd623233465273afb21094ce
cklewar@t-online.de <cklewar@t-online.de>
2022-08-12 19:14:59 +0200

    update example

--- versions.tf
+++ versions.tf
@@ -1,6 +1,14 @@
 terraform {
   required_version = ">= 1.2.7"
   experiments      = [module_variable_optional_attrs]
+  cloud {
+    organization = "cklewar"
+    hostname     = "app.terraform.io"
+
+    workspaces {
+      name = "f5-xc-modules"
+    }
+  }
 
   required_providers {
     f5xc = {
